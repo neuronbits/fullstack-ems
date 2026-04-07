0ff5480c9d78c8a3926859b2ad61b9b6467e8e2a
Nauman <mcs@hotmail.com>
2026-04-07 16:14:05 +0500

    cc

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div>
       <h1 class="text-3xl font-bold underline">
-        Hello world! - welcom
+        Hello world! - welcom.
       </h1>
     </div>
   )
