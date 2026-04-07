010af9dd23a0e9bb11648a06364f48c2c4dc8ec4
Nauman <mcs@hotmail.com>
2026-04-07 15:37:11 +0500

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+//https://www.youtube.com/watch?v=dlu8PmKXopU&list=PPSV&t=6s
+
+const App = () => {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
